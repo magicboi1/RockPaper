b14a601b3783b211fc057124091b2f314068d96b
VLEE <vince_000@hotmail.com>
2022-04-04 20:55:17 -0700

    first step

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+const 
+
+
+
+function computerPlay () {
+    const randomNumber= Math.floor(Math.random()*3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scisscors';
+
+
+    
+};
+
+}
+
+console.log(computerPlay());
